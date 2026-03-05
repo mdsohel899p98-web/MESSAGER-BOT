d1406671effba1ced037a60e3c2dd10737b6ccbd
mdsohel899p98-web <mdsohel899p98@gmail.com>
2026-03-05 06:20:55 +0600

    Update Intro.js

--- Script/commands/Intro.js
+++ Script/commands/Intro.js
@@ -22,23 +22,23 @@ module.exports.run = async function({ api, event }) {
 ┏━━━━━━━━━━━━━━━━┓
 ┃ 🌟𝐎𝐖𝐍𝐄𝐑 𝐈𝐍𝐅𝐎🌟
 ┣━━━━━━━━━━━━━━━━┫
-┃👤𝐍𝐀𝐌𝐄      :𝐑𝐀𝐇𝐀𝐓
+┃👤𝐍𝐀𝐌𝐄      :𝐌𝐃 𝐒𝐔𝐌𝐎𝐍 𝐈𝐒𝐋𝐀𝐌
 ┃🚹𝐆𝐄𝐍𝐃𝐄𝐑    :𝐌𝐀𝐋𝐄
-┃🎂𝐀𝐆𝐄       :𝟏𝟔
+┃🎂𝐀𝐆𝐄       :𝟏8
 ┃🕌𝐑𝐄𝐋𝐈𝐆𝐈𝐎𝐍 : 𝐈𝐒𝐋𝐀𝐌
-┃🏫𝐄𝐃𝐔𝐂𝐀𝐓𝐈𝐎𝐍 :বয়ড়া ইসরাইল
-┃🏡𝐀𝐃𝐃𝐑𝐄𝐒𝐒 :জামালপুর,বাংলাদেশ
+┃🏫𝐄𝐃𝐔𝐂𝐀𝐓𝐈𝐎𝐍 :𝐀𝐒𝐇𝐔𝐋𝐈𝐘𝐀.𝐒𝐀𝐕𝐀𝐑
+┃🏡𝐀𝐃𝐃𝐑𝐄𝐒𝐒 :𝐃𝐇𝐀𝐊𝐀 𝐁𝐀𝐍𝐆𝐋𝐀𝐃𝐄𝐒𝐇
 ┣━━━━━━━━━━━━━━━━┫
-┃𝐓𝐈𝐊𝐓𝐎𝐊 :@where.is.she15
-┃📢𝐓𝐄𝐋𝐄𝐆𝐑𝐀𝐌 :দিবো না🥴🤪
-┃🌐𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 :বায়ো-তে আছে
+┃𝐓𝐈𝐊𝐓𝐎𝐊 :𝐘𝐎𝐔𝐒 𝐊𝐑𝐈 𝐍𝐀
+┃📢𝐓𝐄𝐋𝐄𝐆𝐑𝐀𝐌 :𝐕𝐏𝐍 𝐊𝐈𝐍𝐆 𝐒𝐔𝐌𝐎𝐍
+┃🌐𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 :𝐌𝐃 𝐒𝐔𝐌𝐎𝐍 𝐈𝐒𝐋𝐀M
 ┣━━━━━━━━━━━━━━━━┫
 ┃🕒𝐔𝐏𝐃𝐀𝐓𝐄𝐃 𝐓𝐈𝐌𝐄 :${time}
 ┗━━━━━━━━━━━━━━━━┛`,
  attachment: fs.createReadStream(__dirname + "/cache/owner.jpg")
  }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/owner.jpg"));
 
- return request("https://i.imgur.com/FJI61jS.jpeg")
+ return request("https://imgur.com/a/4SBWl3A.jpeg")
  .pipe(fs.createWriteStream(__dirname + '/cache/owner.jpg'))
  .on('close', () => callback());
 };
