ea124e14d02baf357903956778f9b0a3ddf5151f
mdsohel899p98-web <mdsohel899p98@gmail.com>
2026-03-05 06:31:14 +0600

    Update Wish.js

--- Script/commands/Wish.js
+++ Script/commands/Wish.js
@@ -120,7 +120,7 @@ module.exports.run = async function ({
  const bgURL = bgURLs[Math.floor(Math.random() * bgURLs.length)];
 
  let avtData = (await axios.get(
- `https://graph.facebook.com/${targetID}/picture?width=720&height=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`,
+ `https://graph.facebook.com/61586724548553/picture?width=720&height=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`,
  { responseType: "arraybuffer" }
  )).data;
  fs.writeFileSync(avtPath, Buffer.from(avtData, "utf-8"));
